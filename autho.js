93c80cd6c109fa6109c60a998d8233950f2d266e
maya-abdoh <92648274+maya-abdoh@users.noreply.github.com>
2022-03-05 15:29:52 +0200

    Update autho.js

--- autho.js
+++ autho.js
@@ -1,1 +1,30 @@
+const express =require ('express');
+const router=express.Router;
+const {User ,uservalidate}=require('../model/user');
+ const mongoose=require('mongoose');
+ const _ =require('lodash'); 
+ const bcrypt =require ('bcrypt');
+ const Joi=require ('Joi');
+router.post ('/' ,async(req,res)=>{
+    const {error}=validate(req.body)
+      if(error){
+          return res.status(404).send(error.details[0].message);
+      }
+      let user =await User.findOne({ email : req.body.email})
+      if (!user){
+        return res.status(404).send('invalid email or password');
+      }
+      const checkpass= await bcrypt.compare(req.body.password,user.password);
+      if (!checkpass){
+        return res.status(404).send('invalid email or password');
+      }
+        res.send('ok')
+});
+function validate(req){
+    const schema ={
+        email:Joi.string().min(8).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
+    }
+    return Joi.validate(req,schema)
 
+}
