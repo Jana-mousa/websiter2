e8253850aa11b643492d7685adc569c80ed8c8d9
Rama Hasiba <82103564+Rama-Has@users.noreply.github.com>
2022-04-01 22:36:34 +0300

    Delete auth.js

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const express=require("express");
-const rotuer=express.Router();
- const{signup,activateAccount}=reguire("../rotur/users");
-
- Router.post('/signup-activate',activateAccount)
-
- module.exports=router;
-
